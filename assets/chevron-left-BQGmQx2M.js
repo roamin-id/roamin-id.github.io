57bb14e3340d81c4a33aab108bdf584fcbb642e9
adiputrayasa <adiputrayasa09@gmail.com>
2024-12-23 08:12:50 +0800

    update website

---
+++ assets/chevron-left-BQGmQx2M.js
@@ -0,0 +1,21 @@
+import{r as o}from"./main-DqDa_6Ts.js";/**
+ * @license lucide-react v0.358.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var w={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v0.358.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase();/**
+ * @license lucide-react v0.358.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=(e,s)=>{const a=o.forwardRef(({color:c="currentColor",size:r=24,strokeWidth:n=2,absoluteStrokeWidth:i,className:u="",children:t,...l},m)=>o.createElement("svg",{ref:m,...w,width:r,height:r,stroke:c,strokeWidth:i?Number(n)*24/Number(r):n,className:["lucide",`lucide-${h(e)}`,u].join(" "),...l},[...s.map(([d,p])=>o.createElement(d,p)),...Array.isArray(t)?t:[t]]));return a.displayName=`${e}`,a};/**
+ * @license lucide-react v0.358.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const g=f("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{g as C,f as c};
---
+++ assets/page-head-BYYcFZgy.js
@@ -0,0 +1,1 @@
+import{j as e,I as r}from"./main-DqDa_6Ts.js";function i({title:t="Kutubi"}){return e.jsx(r,{children:e.jsxs("title",{children:[" ",t," "]})})}export{i as P};
